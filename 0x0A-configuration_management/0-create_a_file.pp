236af761b7817c6a2a0cb92e5317c31cb3c8499d
jhojan <jhojan_perlaza123>
2022-08-19 12:21:12 -0500

    [ADD] file 0 and README

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+#  Create a file with Puppet
+file { '/tmp/school': #the path of the new file
+    ensure  => 'present',
+    content => 'I love Puppet', #this text will be inside the file
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+}
