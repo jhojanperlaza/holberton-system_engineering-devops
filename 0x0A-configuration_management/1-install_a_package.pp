3cb02a78ebd1d4e290c4a6e62ecd8202c2bd9456
jhojan <jhojan_perlaza123>
2022-08-19 17:56:36 -0500

    [UPDATED] FILE 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,5 @@
-exec { 'install python packages':
-    command => 'pip3 install flask==2.1.0 flask_restful apiai',
-    path    => ['/usr/bin/'],
+# Using Puppet, install flask from pip3.
+package { 'Install flask':
+  ensure   => '2.1.0', #this is the version
+  provider => 'gem',
 }
