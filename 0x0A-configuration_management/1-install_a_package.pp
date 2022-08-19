436ab04af49782a6a15066ae53bf558abef8d06a
jhojan <jhojan_perlaza123>
2022-08-19 12:55:34 -0500

    [ADD] file 1

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,5 @@
+exec { 'install python packages':
+    command => 'pip3 install flask==2.1.0 flask_restful apiai',
+    path    => ['/usr/bin/'],
+    unless  => '/usr/bin/test -f /usr/local/lib/python3.4/dist-packages/flask/app.py'
+}
