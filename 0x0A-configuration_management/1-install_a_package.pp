f3bc0804109ac6aaab360ff95103f48112b005d7
jhojan <jhojan_perlaza123>
2022-08-19 18:05:00 -0500

    [UPDATED] file 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,8 @@ exec { 'install python packages':
     command => 'pip3 install flask==2.1.0 flask_restful apiai',
     path    => ['/usr/bin/'],
 }
+# other from of install flask:
+# package { 'flask':
+#   ensure   => '2.1.0',
+#   provider => 'pip3',
+# }
