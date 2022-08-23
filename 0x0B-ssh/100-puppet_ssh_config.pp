33c08c5111124cd98f5e7e6b2ee3a0f8eef8ceab
jhojan <jhojan_perlaza123>
2022-08-23 11:38:10 -0500

    [ADD] file 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 #  SSH configuration file so that you can connect
 # to a server without typing a password.
 exec {'Client configuration file (w/ Puppet)':
-    command  => 'echo "Hello World" >> ~/.ssh/school',
+    command  => 'echo "Hello World" >> /etc/ssh/ssh_config',
     provider => shell
 }
