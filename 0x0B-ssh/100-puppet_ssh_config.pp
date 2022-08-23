75b55eac1554ae8bece2a5997a39fc1e92186323
jhojan <jhojan_perlaza123>
2022-08-23 12:36:33 -0500

    [UPDATED]

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,9 @@
 #  SSH configuration file so that you can connect
 # to a server without typing a password.
-exec {' SSH configuration file':
-    command  => '/usr/bin/echo "    IdentityFile ~/.ssh/school" >> ~/etc/ssh/ssh_config',
-    provider => 'shell',
+file {'/etc/ssh/ssh_config':
+  ensure   => 'present',
 }
 exec {' SSH configuration file':
-    command  => '/usr/bin/echo "    PasswordAuthentication no" >> ~/etc/ssh/ssh_config',
-    provider => 'shell',
+  command  => '/usr/bin/echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> ~/etc/ssh/ssh_config',
+  provider => 'shell',
 }
