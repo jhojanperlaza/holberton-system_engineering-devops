e5443421a00839c1b28bba9dc1ce0a26999db8fc
jhojan <jhojan_perlaza123>
2022-09-10 21:52:04 -0500

    [ADD] file 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -9,7 +9,7 @@ import sys
 if __name__ == "__main__":
     url_todos = 'https://jsonplaceholder.typicode.com/todos'
     url_user = 'https://jsonplaceholder.typicode.com/users/{}'.format(
-        sys.argv[1])
+        2)
 
     rq_user_name = requests.get(url_user).json()['username']
     rq_todos = requests.get(url_todos).json()
@@ -19,14 +19,16 @@ if __name__ == "__main__":
     dict_save = {}
 
     for element in rq_todos:
-        if element['userId'] == int(sys.argv[1]):
-            dict_save['task'] = element['title']
-            dict_save['completed'] = element['completed']
-            dict_save['username'] = rq_user_name
+        if element['userId'] == int(2):
+            dict_save = {
+                'task': element['title'],
+                'completed': element['completed'],
+                'username': rq_user_name
+                }
             list_save.append(dict_save)
 
-    format_save[sys.argv[1]] = list_save
+    format_save[2] = list_save
 
-    with open("{}.json".format(sys.argv[1]), 'w',) as file:
+    with open("{}.json".format(2), 'w',) as file:
         str_json = json.dumps(format_save)
         file.write(str_json)
